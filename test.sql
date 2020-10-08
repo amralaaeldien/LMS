cf7492c4411fa35736cbb69469414c467f299d10
Amr Shedou <amrshedou@gmail.com>
2020-10-08 22:07:20 +0200

    modified test.sql

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2020 at 02:55 AM
+-- Generation Time: Oct 08, 2020 at 10:06 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -45,6 +45,52 @@ INSERT INTO `courses` (`id`, `title`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `content` text DEFAULT NULL,
+  `sender` int(11) DEFAULT NULL,
+  `receiver` int(11) DEFAULT NULL,
+  `sent_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `content`, `sender`, `receiver`, `sent_at`) VALUES
+(15, 'hi', 21, 1, '2020-10-08 19:35:26'),
+(16, 'hi there', 1, 21, '2020-10-08 19:52:41'),
+(17, 'hi', 24, 21, '2020-10-08 19:54:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `user` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_clicked` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `user`, `description`, `created_at`, `is_clicked`) VALUES
+(2, 1, 'user2 sent you a message at 2020-10-08 21:35:26', '2020-10-08 19:35:26', 0),
+(3, 21, 'user1 sent you a message at 2020-10-08 21:52:41', '2020-10-08 19:52:42', 0),
+(4, 21, 'user3 sent you a message at 2020-10-08 21:54:06', '2020-10-08 19:54:06', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `steps`
 --
 
@@ -67,6 +113,32 @@ INSERT INTO `steps` (`id`, `title`, `description`, `course`, `is_done`) VALUES
 (4, 'defining functions in javascript', 'write the function keyword then the name of the function', 2, NULL),
 (5, 'how to define variables in ruby', 'i don\'t know how', 3, 0),
 (6, 'how to define functions in ruby', 'I don\'t know fucking how', 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `steps_and_users`
+--
+
+CREATE TABLE `steps_and_users` (
+  `step_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `is_done` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `steps_and_users`
+--
+
+INSERT INTO `steps_and_users` (`step_id`, `user_id`, `is_done`) VALUES
+(1, 1, 0),
+(1, 21, 0),
+(2, 1, 0),
+(2, 21, 1),
+(3, 1, 0),
+(4, 1, 0),
+(5, 1, 0),
+(6, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -100,11 +172,33 @@ ALTER TABLE `courses`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sender` (`sender`),
+  ADD KEY `receiver` (`receiver`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
 -- Indexes for table `steps`
 --
 ALTER TABLE `steps`
   ADD PRIMARY KEY (`id`),
   ADD KEY `course` (`course`);
+
+--
+-- Indexes for table `steps_and_users`
+--
+ALTER TABLE `steps_and_users`
+  ADD PRIMARY KEY (`step_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -124,6 +218,18 @@ ALTER TABLE `courses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `steps`
 --
 ALTER TABLE `steps`
@@ -140,10 +246,30 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `steps`
 --
 ALTER TABLE `steps`
   ADD CONSTRAINT `steps_ibfk_1` FOREIGN KEY (`course`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `steps_and_users`
+--
+ALTER TABLE `steps_and_users`
+  ADD CONSTRAINT `steps_and_users_ibfk_1` FOREIGN KEY (`step_id`) REFERENCES `steps` (`id`),
+  ADD CONSTRAINT `steps_and_users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
